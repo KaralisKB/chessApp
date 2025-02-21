58286115444dff78528e10df7305487de520c091
KaralisKB <kristianbariss@gmail.com>
2025-02-21 10:50:47 +0000

    Added lottie dependency and modified process of implementation of new libraries.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,26 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
 }
+
+buildscript {
+    dependencies {
+        classpath(libs.gradle)
+    }
+    repositories {
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://maven.aliyun.com/repository/jcenter") }
+        mavenCentral()
+        google()
+    }
+}
+
+allprojects {
+    repositories {
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://maven.aliyun.com/repository/jcenter") }
+        mavenCentral()
+        google()
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,11 +12,7 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
 }
 
 rootProject.name = "Chess"
